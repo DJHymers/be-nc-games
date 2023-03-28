610e854b4e0fea996c7f77bd4d67b65e5001b3d9
DJHymers <david_hymers@yahoo.com>
2023-03-28 17:04:49 +0100

    Added path to request reviews by review id along with improved error handling.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,47 +1,87 @@
 const connection = require(`${__dirname}/../db/connection.js`);
 const app = require(`${__dirname}/../app.js`);
-const Data = require(`${__dirname}/../db/data/test-data/index.js`);
+const data = require(`${__dirname}/../db/data/test-data/index.js`);
 const seed = require(`${__dirname}/../db/seeds/seed.js`);
 const request = require("supertest");
 
 beforeEach(() => {
-  return seed(Data);
+  return seed(data);
 });
 
 afterAll(() => connection.end());
 
-describe("/api/categories", () => {
-  it("200: GET /api/categories should respond with array of category objects", () => {
-    return request(app)
-      .get("/api/categories")
-      .expect(200)
-      .then(({ body }) => {
-        const { categories } = body;
-
-        expect(categories).toBeInstanceOf(Array);
-        categories.forEach((category) => {
-          expect(category).toBeInstanceOf(Object);
+describe("nc_games_test", () => {
+  describe("/api/categories", () => {
+    it("200: GET /api/categories should respond with array of category objects", () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then(({ body }) => {
+          const { categories } = body;
+          expect(categories).toBeInstanceOf(Array);
+          categories.forEach((category) => {
+            expect(category).toBeInstanceOf(Object);
+          });
         });
-      });
-  });
-  it("200: GET /api/categories should have expected shape", () => {
-    return request(app)
-      .get("/api/categories")
-      .then(({ body }) => {
-        const { categories } = body;
-        expect(categories.length).toBe(4);
-        categories.forEach((category) => {
-          expect(category).toHaveProperty("slug", expect.any(String));
-          expect(category).toHaveProperty("description", expect.any(String));
+    });
+    it("200: GET /api/categories should have expected shape", () => {
+      return request(app)
+        .get("/api/categories")
+        .then(({ body }) => {
+          const { categories } = body;
+          expect(categories.length).toBe(4);
+          categories.forEach((category) => {
+            expect(category).toHaveProperty("slug", expect.any(String));
+            expect(category).toHaveProperty("description", expect.any(String));
+          });
         });
-      });
+    });
+    it("404: Should respond with invalid path message if given invalid path input", () => {
+      return request(app)
+        .get("/api/bananas")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("404 path not found");
+        });
+    });
   });
-  it("404: Should respond with invalid path message if given invalid path input", () => {
-    return request(app)
-      .get("/api/bananas")
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.msg).toBe("404 path not found");
-      });
+  describe("/api/reviews/:review_id", () => {
+    it("200: GET /api/reviews/:review_id should return relevant review_id data", () => {
+      return request(app)
+        .get("/api/reviews/3")
+        .expect(200)
+        .then(({ body }) => {
+          const { reviews } = body;
+          expect(reviews).toBeInstanceOf(Object);
+          expect(reviews).toEqual({
+            review_id: 3,
+            title: "Ultimate Werewolf",
+            review_body: "We couldn't find the werewolf!",
+            designer: "Akihisa Okui",
+            review_img_url:
+              "https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?w=700&h=700",
+            votes: 5,
+            category: "social deduction",
+            owner: "bainesface",
+            created_at: "2021-01-18T10:01:41.251Z",
+          });
+        });
+    });
+    it("404: Should respond with invalid path error if input is invalid id number", () => {
+      return request(app)
+        .get("/api/reviews/212121221")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("404 path not found");
+        });
+    });
+    it("400: invalid id type provided, should provide bad request message", () => {
+      return request(app)
+        .get("/api/reviews/bananas")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).toBe("Invalid input");
+        });
+    });
   });
 });
--- controllers/errorHandling.controller.js
+++ controllers/errorHandling.controller.js
@@ -1,3 +1,22 @@
 exports.invalidPathError = (req, res, next) => {
   res.status(404).send({ msg: "404 path not found" });
 };
+
+exports.customErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+
+exports.handlePsqlErrors = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid input" });
+  } else next(err);
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal Server Error" });
+};
